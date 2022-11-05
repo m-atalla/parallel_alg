2972f3ee9b1e617affac0c6b9af1971183443250
m-atalla <mo.atalla96@gmail.com>
2022-11-05 14:05:32 +0200

    add thread pool module rename Point trait add print_clusters! macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 pub mod sequential;
 pub mod parallel;
+pub mod threads;
 
 
-pub trait Contained {
+pub trait Constructed {
     fn new(x: f64, y: f64) -> Self;
+}
+
+#[macro_export]
+macro_rules! print_clusters {
+    ($clusters: expr) => {
+        println!("\nFinal clusters:");
+        for cluster in $clusters {
+            println!("\t{:?}", cluster);
+        }
+    };
 }
 
 pub const MAX_ITER: usize = 10000;
