ac0d8c19bdb623e29ca17f499e6d9e75c26b2357
m-atalla <mo.atalla96@gmail.com>
2022-11-19 01:53:03 +0200

    add number arg parsing lib

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,6 +1,5 @@
 pub use std::time::Instant;
 
-
 #[macro_export]
 /// Profiles an expression 
 macro_rules! time_eval {
@@ -15,4 +14,21 @@ macro_rules! time_eval {
 
         print!("Done!, Elapsed: {:.2?}\n", elapsed);
     };
+}
+
+
+
+pub fn parse_usize_flag(flag_name: &str, default_value: usize, iter: &mut impl Iterator<Item = String>) -> usize {
+    match iter.next() {
+        Some(num_arg) => {
+            num_arg.parse::<usize>().unwrap_or_else(|_| {
+                eprintln!("Expected a numeric argument after `{flag_name}` flag, got: {}", num_arg);
+                default_value
+            })
+        },
+        None => {
+            eprintln!("Missing argument after `-k` flag, using default {flag_name}={default_value}");
+            default_value
+        }
+    }
 }
